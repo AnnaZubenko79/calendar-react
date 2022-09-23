5a08d687341f102cee7da676ed926d49e69f15e3
Anna Zubenko <annazubenko79@gmail.com>
2022-09-24 00:32:43 +0300

    change webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const webpack = require('webpack');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = (env, argv) => {
   const isProduction = argv.mode === 'production';
@@ -10,6 +11,7 @@ module.exports = (env, argv) => {
     output: {
       filename: 'bundle.js',
     },
+
     module: {
       rules: [
         {
@@ -35,6 +37,9 @@ module.exports = (env, argv) => {
       new CleanWebpackPlugin(),
       new HtmlWebpackPlugin({
         template: './src/index.html',
+      }),
+      new CopyPlugin({
+        patterns: [{ from: '_redirects', to: '' }],
       }),
     ],
     devServer: {
